e27599f3a98cd12b9d3723781a9e8e457deb0aff
Emmanuel GRENIER (BtB) <boxtobiker@gmail.com>
2025-10-05 11:46:11 +0200

    feat(btb) use notosans font replacing creatoDisplay

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'generated/l10n.dart';
 import 'features/home/ui/pages/home.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Configuration pour améliorer le rendu des polices sur Android
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,10 +28,30 @@ class MyApp extends StatelessWidget {
       ],
       supportedLocales: S.delegate.supportedLocales,
       title: 'BoxtoBikers',
-      // The Big stone, light theme. Other gold
-      theme: FlexThemeData.light(scheme: FlexScheme.bigStone, fontFamily: 'NotoSans'),
-      // The Big stone, dark theme. Other gold
-      darkTheme: FlexThemeData.dark(scheme: FlexScheme.bigStone, fontFamily: 'NotoSans'),
+      // The Big stone, light theme
+      theme: FlexThemeData.light(
+        scheme: FlexScheme.bigStone,
+        fontFamily: 'NotoSans',
+        useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        // Configuration pour améliorer le rendu des polices sur Android
+        subThemesData: const FlexSubThemesData(
+          useMaterial3Typography: true,
+          defaultRadius: 8.0,
+        ),
+      ),
+      // The Big stone, dark theme
+      darkTheme: FlexThemeData.dark(
+        scheme: FlexScheme.bigStone,
+        fontFamily: 'NotoSans',
+        useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        // Configuration pour améliorer le rendu des polices sur Android
+        subThemesData: const FlexSubThemesData(
+          useMaterial3Typography: true,
+          defaultRadius: 8.0,
+        ),
+      ),
       themeMode: ThemeMode.system,
       home: Builder(
         builder: (context) => MyHomePage(title: S.of(context).homeTitle),
